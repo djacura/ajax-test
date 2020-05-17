9534be2b476b4d13d6ca1cd2388d4a656c42f5ef
Daryl <d.jacura@sky.com>
2020-05-17 08:14:05 +0000

    changed table data js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-function getData(cb) {
+const baseURL = "https://ci-swapi.herokuapp.com/api/"
+
+function getData(type, cb) {
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+  xhr.open("GET", baseURL + type + "/");
   xhr.send();
 
   xhr.onreadystatechange = function () {
@@ -11,8 +13,36 @@ function getData(cb) {
   };
 }
 
-function printDataToConsole(data) {
-    console.log(data);
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`);
+    })
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
-getData(printDataToConsole);
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function(data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function(item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function(key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    })
+}
